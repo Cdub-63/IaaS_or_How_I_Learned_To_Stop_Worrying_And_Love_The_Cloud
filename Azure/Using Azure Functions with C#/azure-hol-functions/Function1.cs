4eadb9b9875cde6f0c6848dd78b99559552ceb7b
Christian <44756128+Cdub-63@users.noreply.github.com>
2021-03-31 14:37:25 -0500

    Add files via upload

---
+++ Azure/Using Azure Functions with C#/azure-hol-functions/Function1.cs
@@ -0,0 +1,10 @@
+namespace azure_hol_functions
+{
+    using System;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.WindowsAzure.Storage.Table;
+
+}
